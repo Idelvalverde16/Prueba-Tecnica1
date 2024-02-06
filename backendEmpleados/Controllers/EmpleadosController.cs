cf6aa9bff1fc41967e3edc653aada854ae23e490
Administrator <Administrator@JAEGER>
2024-02-06 16:57:13 -0400

    Add project files.

---
+++ backendEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendEmpleados.Controllers
+{
+
+[ApiController]
+    [Route("api/[controller]")]
+    public class EmpleadosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmpleadosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Empleados
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Empleado>>> GetEmpleados()
+        {
+            return await _context.Empleados.ToListAsync();
+        }
+
+        // GET: api/Empleados/1
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Empleado>> GetEmpleados(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return empleado;
+        }
+
+        // POST: api/Empleados
+        [HttpPost]
+        public async Task<ActionResult<Empleado>> PostEmpleado(Empleado empleado)
+        {
+            _context.Empleados.Add(empleado);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmpleados), new { id = empleado.Id }, empleado);
+        }
+
+        // PUT: api/Empleados
+        [HttpPut ("{id}")]
+        public async Task<IActionResult> PutEmpleado(int id, Empleado empleado)
+        {
+            if (id != empleado.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(empleado).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpleadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+               
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Empleados
+        [HttpDelete ("{id}")]
+        public async Task<IActionResult> DeletePlayer(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            _context.Empleados.Remove(empleado);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+            return _context.Empleados.Any(e => e.Id == id);
+        }
+
+    }
+}
---
+++ backendEmpleados/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backendEmpleados
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+        [MaxLength(60)]
+
+        public string Departamento { get; set; }
+        [MaxLength(60)]
+
+        public string Cargo { get; set; }
+        [MaxLength(60)]
+
+        public string Genero { get; set; }
+        [MaxLength(10)]
+
+        public decimal Salario { get; set; }
+    }
+}
